22d715a8e8626dd14c3ffcd1a7587710c73d31bb
Brecht Hoflack <brh@melexis.com>
2011-03-14 16:05:22 +0100

    Serialize the lot to xml instead of yaml. Use the lot model instead of a custom model.

--- postprocessing.pl
+++ postprocessing.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/env perl
-#use strict;
+
+use strict;
 use MIME::Base64;
 use Net::Stomp;
-use YAML;
+use XML::Simple;
+use Data::Dumper;
 
 # Open a file and return the full contents.
 sub read_file {
@@ -14,15 +16,26 @@ sub read_file {
 
 # Create a wafermaps hash for the given lot.
 sub wafermaps {
-    my $lotname = shift;
+    my $lot = shift;
+    my $lotname = $lot->{lot}->{name};
 
-    return {
-        lotname => $lotname,
-        converted_at => "20110228 14:01",
-        wafermaps => {
-          1 => read_file("example.th01"),
-          2 => read_file("example.th01"),
-        }};
+    for my $wafer(@{$lot->{lot}->{wafer}}) {
+        # Create a wafermap hash for the postprocessing map
+        my $wafermap = { 'name' => 'postprocessing',
+                         'formats' => {
+                             'format' => [{
+                                 'name' => 'th01',
+                                 'content' => read_file("example.th01")
+                             }]
+                         }
+        };
+
+        # Add the wafermap to the list of wafermaps
+        push @{$wafer->{wafermap}}, $wafermap;
+    }
+
+
+    return $lot;
 }
 
 my $stomp = Net::Stomp->new({ hostname => "localhost",
@@ -37,12 +50,22 @@ $stomp->subscribe({ "destination" => "/queue/postprocessing",
 while (1) {
     my $frame = $stomp->receive_frame;
 
-    my %contents = Load($frame->body());
-    my $lotname = $contents{"lotname"};
+    # Read the xml in a hash ref
+    # KeepRoot keeps the lot root as it is,
+    # by specifying an empty KeyAttr we ensure that the structure
+    # isn't changed.
+    # ForceArray is set to true so that the format key stays available.
+    my $lot = XMLin($frame->body, KeepRoot => 1, KeyAttr => undef, ForceArray => ["format"]);
+
+    my $processed = wafermaps($lot);
+
+    # convert back to xml,  RootName is set to undef so that
+    # XML::Simple doesn't add the opt tags
+    my $body = XMLout($processed, RootName => undef, GroupTags => undef);
 
     # Send the result back
     $stomp->send({ destination => "/queue/postprocessing_results",
-                   body => Dump(wafermaps($lotname)) });
+                   body => $body });
 
     # Acknowledge the message
     $stomp->ack( { frame => $frame } );
